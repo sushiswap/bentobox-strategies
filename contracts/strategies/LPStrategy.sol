aed144c4e7edd2d581fa7b6fd420193f86501421
0xCalibur <0xCalibur@protonmail.com>
2021-10-20 13:22:50 -0400

    Merge branch 'master' of github.com:Abracadabra-money/bentobox-strategies

--- contracts/strategies/LPStrategy.sol
+++ contracts/strategies/LPStrategy.sol
@@ -2,52 +2,88 @@
 
 pragma solidity 0.8.7;
 
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../BaseStrategy.sol";
 import "../interfaces/ISushiSwap.sol";
 import "../libraries/Babylonian.sol";
 
 interface IMasterChef {
     function deposit(uint256 _pid, uint256 _amount) external;
-    // Withdraw LP tokens from MasterChef.
+
     function withdraw(uint256 _pid, uint256 _amount) external;
-    function userInfo(uint256 _pid, address user) external returns(uint256 amount, uint256 rewardDebt);
+
+    function userInfo(uint256 _pid, address user)
+        external
+        returns (uint256 amount, uint256 rewardDebt);
+
     function emergencyWithdraw(uint256 _pid) external;
 }
 
 contract LPStrategy is BaseStrategy {
-    address constant sushiSwapFactory = 0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac; // SushiSwap factory contract
-    address constant wETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // ETH wrapper contract v9
-    ISushiSwap constant sushiSwapRouter = ISushiSwap(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F); // SushiSwap router contract
-    uint256 constant deadline = 0xf000000000000000000000000000000000000000000000000000000000000000; // ~ placeholder for swap deadline
-    IMasterChef public immutable masterchef;
+    using SafeERC20 for IERC20;
+
+    uint256 constant deadline =
+        0xf000000000000000000000000000000000000000000000000000000000000000; // ~ placeholder for swap deadline
+
+    ISushiSwap private immutable router;
+    IMasterChef private immutable masterchef;
     uint256 private immutable pid;
+    address private immutable rewardToken;
+    bool private immutable usePairToken0;
+    address private immutable pairInputToken;
+
+    /** @param _strategyToken Address of the underlying LP token the strategy invests.
+        @param _bentoBox BentoBox address.
+        @param _factory SushiSwap factory.
+        @param _bridgeToken An intermediary token for swapping any rewards into it before swapping it to _inputPairToken
+        @param _strategyExecutor an EOA that will execute the safeHarvest function.
+        @param _usePairToken0 When true, the _rewardToken will be swapped to the pair's token0 for one-sided liquidity
+                                providing, otherwise, the pair's token1.
+    */
     constructor(
         address _strategyToken,
         address _bentoBox,
-        address _strategyExecutor,
+        address _factory,
         address _bridgeToken,
+        address _strategyExecutor,
         IMasterChef _masterchef,
-        uint256 _pid
-    ) BaseStrategy(_strategyToken, _bentoBox, sushiSwapFactory, _bridgeToken, _strategyExecutor)  {
+        uint256 _pid,
+        ISushiSwap _router,
+        address _rewardToken,
+        bool _usePairToken0
+    )
+        BaseStrategy(
+            _strategyToken,
+            _bentoBox,
+            _factory,
+            _bridgeToken,
+            _strategyExecutor
+        )
+    {
         masterchef = _masterchef;
         pid = _pid;
+        router = _router;
+        rewardToken = _rewardToken;
+
         (address token0, address token1) = _getPairTokens(_strategyToken);
-        IERC20(token0).safeApprove(
-            address(sushiSwapRouter),
-            type(uint256).max
-        );
-        IERC20(token1).safeApprove(
-            address(sushiSwapRouter),
-            type(uint256).max
-        );
+        IERC20(token0).safeApprove(address(_router), type(uint256).max);
+        IERC20(token1).safeApprove(address(_router), type(uint256).max);
+
+        usePairToken0 = _usePairToken0;
+        pairInputToken = _usePairToken0 ? token0 : token1;
     }
 
     function _skim(uint256 amount) internal override {
         masterchef.deposit(pid, amount);
     }
 
-    function _harvest(uint256 balance) internal override returns (int256 amountAdded) {
+    function _harvest(uint256 /* balance */)
+        internal
+        override
+        returns (int256 amountAdded)
+    {
         masterchef.withdraw(pid, 0);
+        amountAdded = _swapToLp();
     }
 
     function _withdraw(uint256 amount) internal override {
@@ -58,118 +94,96 @@ contract LPStrategy is BaseStrategy {
         masterchef.emergencyWithdraw(pid);
     }
 
-    function _getPairTokens(address _pairAddress) private pure returns (address token0, address token1)
+    function _getPairTokens(address _pairAddress)
+        private
+        pure
+        returns (address token0, address token1)
     {
         ISushiSwap sushiPair = ISushiSwap(_pairAddress);
         token0 = sushiPair.token0();
         token1 = sushiPair.token1();
     }
 
-    function _swapFirst(address inputToken) internal returns (uint256 amountOut, address _bridgeToken) {
+    function _swapTokensForUnderlying(address tokenIn, address tokenOut)
+        private
+        returns (uint256 amountOut)
+    {
+        bool useBridge = bridgeToken != address(0);
+        address[] memory path = new address[](useBridge ? 3 : 2);
 
-        address[] memory path = new address[](2);
-
-        path[0] = inputToken;
-
-        _bridgeToken = bridgeToken;
-
-        path[1] = _bridgeToken;
+        path[0] = tokenIn;
+        if (useBridge) {
+            path[1] = bridgeToken;
+        }
+        path[path.length - 1] = tokenOut;
 
         uint256 amountIn = IERC20(path[0]).balanceOf(address(this));
-
-        uint256[] memory amounts = UniswapV2Library.getAmountsOut(factory, amountIn, path);
-
+        uint256[] memory amounts = UniswapV2Library.getAmountsOut(
+            factory,
+            amountIn,
+            path
+        );
         amountOut = amounts[amounts.length - 1];
 
-        IERC20(path[0]).safeTransfer(UniswapV2Library.pairFor(factory, path[0], path[1]), amounts[0]);
+        IERC20(path[0]).safeTransfer(
+            UniswapV2Library.pairFor(factory, path[0], path[1]),
+            amounts[0]
+        );
 
         _swap(amounts, path, address(this));
     }
 
-    function _swapIntermediate(
-        address tokenFrom,
-        address _ToSushipoolToken0,
-        address _ToSushipoolToken1,
-        uint256 _amount,
-        address _pairAddress
-    ) private returns (uint256 token0Bought, uint256 token1Bought) {
-        ISushiSwap pair = ISushiSwap(_pairAddress);
-        (uint256 res0, uint256 res1, ) = pair.getReserves();
-
-        uint256 amountIn;
-        address[] memory path = new address[](useBridge ? 3 : 2);
-        path[0] = tokenFrom;
-        uint256[] memory amounts;
-
-        if (tokenFrom == _ToSushipoolToken0) {
-            amountIn = calculateSwapInAmount(res0, _amount);
-            path[1] = _ToSushipoolToken1;
-            amounts = UniswapV2Library.getAmountsOut(factory, amountIn, path);
-            token1Bought = amounts[amounts.length - 1];
-            token0Bought = _amount - amountToSwap;
-        } else {
-            amountIn = calculateSwapInAmount(res1, _amount);
-            path[1] = _ToSushipoolToken1;
-            amounts = UniswapV2Library.getAmountsOut(factory, amountIn, path);
-            token0Bought = amounts[amounts.length - 1];
-            token1Bought = _amount - amountToSwap;
-        }
-
-        IERC20(path[0]).safeTransfer(UniswapV2Library.pairFor(factory, path[0], path[1]), amounts[0]);
-
-        _swap(amounts, path, address(this));
-    }
-
-    function calculateSwapInAmount(uint256 reserveIn, uint256 userIn) private pure returns (uint256)
+    function _calculateSwapInAmount(uint256 reserveIn, uint256 userIn)
+        private
+        pure
+        returns (uint256)
     {
         return
-            (Babylonian
-                .sqrt(
+            (Babylonian.sqrt(
                 reserveIn * userIn * 3988000 + reserveIn * 3988009
-            )
-                - reserveIn * 1997) / 1994;
+            ) - reserveIn * 1997) / 1994;
     }
 
     /// @notice Swap some tokens in the contract for the underlying and deposits them to address(this)
-    function swapToLP(uint256 amountOutMin, address inputToken) public onlyExecutor returns (uint256 amountOut) {
-        uint256 intermediateAmt;
-        address intermediateToken;
-        address _pairAddress = strategyToken;
-        (
-            address _ToSushipoolToken0,
-            address _ToSushipoolToken1
-        ) = _getPairTokens(_pairAddress);
-
-        if (
-            _FromTokenContractAddress != _ToSushipoolToken0 &&
-            _FromTokenContractAddress != _ToSushipoolToken1
-        ) {
-            // swap to intermediate
-            (intermediateAmt, intermediateToken) = _swapFirst(inputToken);
-        } else {
-            intermediateToken = inputToken;
-            intermediateAmt = IERC20(inputToken).balanceOf(address(this));
-        }
-        // divide intermediate into appropriate amount to add liquidity
-        (uint256 token0Bought, uint256 token1Bought) = _swapIntermediate(
-            intermediateToken,
-            _ToSushipoolToken0,
-            _ToSushipoolToken1,
-            _pairAddress
+    function _swapToLp() private returns (int256 amountOut) {
+        uint256 tokenInAmount = _swapTokensForUnderlying(rewardToken, pairInputToken);
+        (uint256 reserve0, uint256 reserve1, ) = ISushiSwap(strategyToken).getReserves();
+        (address token0, address token1) = _getPairTokens(strategyToken);
+        
+        // The pairInputToken amount to swap to get the equivalent pair second token amount
+        uint256 swapAmountIn = _calculateSwapInAmount(
+            tokenInAmount,
+            usePairToken0 ? reserve0 : reserve1
         );
 
-        (, uint256 LPBought) = sushiSwapRouter
-            .addLiquidity(
-            _ToSushipoolToken0,
-            _ToSushipoolToken1,
-            token0Bought,
-            token1Bought,
+        address[] memory path = new address[](2);
+        if(usePairToken0) {
+            path[0] = token0;
+            path[1] = token1;
+        } else {
+            path[0] = token1;
+            path[1] = token0;
+        }
+
+        uint256[] memory amounts = UniswapV2Library.getAmountsOut(factory, swapAmountIn, path);
+        IERC20(path[0]).safeTransfer(strategyToken, amounts[0]);
+        _swap(amounts, path, address(this));
+        uint256 pairInputTokenAmount = IERC20(pairInputToken).balanceOf(address(this));
+
+        uint256 amountStrategyLpBefore = IERC20(strategyToken).balanceOf(address(this));
+        router.addLiquidity(
+            token0,
+            token1,
+            usePairToken0 ? pairInputTokenAmount : amounts[0],
+            usePairToken0 ? amounts[0] : pairInputTokenAmount,
             1,
             1,
             address(this),
             deadline
         );
 
-        require(LPBought >= amountOutMin, 'ERR: High Slippage');
+        uint256 amountOutUnsigned = IERC20(strategyToken).balanceOf(address(this)) - amountStrategyLpBefore;
+        require(amountOutUnsigned <= uint256(type(int256).max), "SafeCast: value doesn't fit in an int256");
+        amountOut = int256(amountOutUnsigned);
     }
 }
